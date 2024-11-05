0d5e743ccf92ba78b834716a2baefeb83007d995
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2024-11-05 16:01:11 +0800

    CPU: Don't accept candidate until GPU filtering is mostly done

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ import random
 import functools
 import os
 
-from config import IS_DOCKER, SCRIPT_FOLDER, API_DEF_RETRIES, API_MAX_TIMEOUT, API_CANDIDATE_GEN_WAIT_TIME, GIT_VERSION
+from config import IS_DOCKER, SCRIPT_FOLDER, API_DEF_RETRIES, API_MAX_TIMEOUT, API_CANDIDATE_GEN_WAIT_TIME, GIT_VERSION, API_CPU_ACCEPT_THRESHOLD
 from candidate import Candidate
 
 API_TOKEN = open(f"{SCRIPT_FOLDER}/api_token.txt").read().strip()
@@ -29,18 +29,39 @@ def onAPIError(funcName, retriesLeft):
     time.sleep(toWait)
 
 
-def getCandidateFromSisMargaret(retriesLeft = API_DEF_RETRIES):
+def getCandidateAmountOnSisMargaret(retriesLeft = API_DEF_RETRIES):
     while True:
         try:
-            url = SISMARGARET_API_BASE + "candidate/version/1"
-            ret = API_SESSION.get(url).json()
-            if "n" in ret:
-                print(f"getCandidateFromSisMargaret: Got new candidate {ret}")
-                return Candidate(ret["id"], ret["height"], int(ret["n"]))
-            else:
-                waitTime = random.uniform(*API_CANDIDATE_GEN_WAIT_TIME)
-                print(f"getCandidateFromSisMargaret: No candidate available. Retrying in {waitTime:.1f}s")
-                time.sleep(waitTime)
+            url = SISMARGARET_API_BASE + "candidates/remaining"
+            return int(API_SESSION.get(url).text)
+        except Exception:
+            onAPIError("getCandidateAmountOnSisMargaret", retriesLeft)
+            retriesLeft -= 1
+
+
+def getCandidateFromSisMargaret(retriesLeft = API_DEF_RETRIES, skipAmountCheck = False):
+    while True:
+        try:
+            waitReason = None
+            if not skipAmountCheck:
+                amount = getCandidateAmountOnSisMargaret()
+                if amount == 0:
+                    waitReason = "No candidate available"
+                elif amount >= API_CPU_ACCEPT_THRESHOLD:
+                    waitReason = "GPU filter ongoing"
+
+            if waitReason is None:
+                url = SISMARGARET_API_BASE + "candidate/version/1"
+                ret = API_SESSION.get(url).json()
+                if "n" in ret:
+                    print(f"getCandidateFromSisMargaret: Got new candidate {ret}")
+                    return Candidate(ret["id"], ret["height"], int(ret["n"]))
+                else:
+                    waitReason = "No candidate available"
+
+            waitTime = random.uniform(*API_CANDIDATE_GEN_WAIT_TIME)
+            print(f"getCandidateFromSisMargaret: {waitReason}. Retrying in {waitTime:.1f}s")
+            time.sleep(waitTime)
         except Exception:
             onAPIError("getCandidateFromSisMargaret", retriesLeft)
             retriesLeft -= 1
