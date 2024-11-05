cb87d28918406e732d03787de3c6988661acb170
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2024-11-05 16:34:21 +0800

    Allow running *MinerStart.sh from any folder

--- CPUMinerStart.sh
+++ CPUMinerStart.sh
@@ -2,7 +2,9 @@
 IMAGE_NAME=sieverdispersion-cpu
 DOCKERFILE_NAME=Dockerfile-cpu
 
+pushd -- "$(dirname "$0")"
 git pull
-sudo docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME . || exit 1
+sudo docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME . || { popd; exit 1; }
 sudo docker stop $(sudo docker ps -aq -f name=$IMAGE_NAME); sudo docker rm $(sudo docker ps -aq -f name=$IMAGE_NAME)
 sudo docker run --init -it -e YAFU_THREADS=$YAFU_THREADS --name $IMAGE_NAME $IMAGE_NAME
+popd
