06d08509a6a9e447b9d260c3553e259c90e69a91
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2024-11-21 19:16:47 +0800

    Fix *PUMinerStart.sh breaking during first startup

--- GPUMinerStart.sh
+++ GPUMinerStart.sh
@@ -7,6 +7,7 @@ pushd -- "$(dirname "$0")"
 git pull
 ./maybeGenerateMachineID.sh
 sudo docker build -t $IMAGE_NAME -f $DOCKERFILE_NAME . || { popd; exit 1; }
-sudo docker stop $(sudo docker ps -aq -f name=$IMAGE_NAME); sudo docker rm $(sudo docker ps -aq -f name=$IMAGE_NAME)
+sudo docker stop $(sudo docker ps -aq -f name=$IMAGE_NAME) || true
+sudo docker rm $(sudo docker ps -aq -f name=$IMAGE_NAME) || true
 sudo docker run --init -it --gpus all -e YAFU_THREADS=$YAFU_THREADS --name $IMAGE_NAME $IMAGE_NAME
 popd
