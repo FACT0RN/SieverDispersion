2a467cb71df61c3c1470942016c2b9c1b0be4f3a
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2024-11-21 16:09:07 +0800

    Use python3 to generate UUID

--- maybeGenerateMachineID.sh
+++ maybeGenerateMachineID.sh
@@ -3,7 +3,7 @@
 # If machineID.txt does not exist, generate a new one
 if [ ! -f machineID.txt ]; then
     # Use UUID v4
-    uuid=$(uuidgen)
+    uuid=$(python3 -c 'import uuid; print(uuid.uuid4())')
     echo "$uuid" > machineID.txt
     echo "Machine ID generated and saved to machineID.txt"
     echo "Please remove machineID.txt (or move it somewhere else) if you need to move this machine to another account."
