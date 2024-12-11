d330737834e88f5165317c75423d6176989d266e
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2024-12-11 23:22:46 +0800

    Bare metal GPUMinerStart.sh: Check if unbuffer is installed

--- files/GPUMinerStart.sh
+++ files/GPUMinerStart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# If the command unbuffer doesn't exist
+if ! command -v unbuffer >/dev/null 2>&1; then
+    echo "The 'unbuffer' command is not installed. Please install it. You can do this by installing the 'expect' package using your distro's package manager."
+    exit 1
+fi
+
 pushd -- "$(dirname "$0")"
 ./maybeGenerateMachineID.py
 SIEVER_MODE=1 ./SieverDispersion
